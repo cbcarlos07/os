75306023ffe89c18a50afd5f517b508ba8d3beb1
Carlos Bruno <cbcarlos08@gmail.com>
2017-08-25 12:37:12 -0400

    First Commit

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
 
         <div class="copy text-center">
-            Copyright 2017 <a href='#'>Departamento de Tecnologia de Informa&ccedil;&atilde;o</a>
+            Copyright 2017 <a href='#'>Departamento de Tecnologia da Informa&ccedil;&atilde;o v1.0</a>
         </div>
 
     </div>
